a9b9f72204d7a849a9eba2b015041084451aaebf
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-18 13:13:17 +0100

    Adding main file

---
+++ HostFile.py
@@ -0,0 +1,6 @@
+# Attempting to make a basic chess game whereby the computer randomly selects a next move.
+# This will form the framework for a later project that looks to pit a manual algorythm against a neaural network (probably NEAT)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
