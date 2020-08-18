4a803254f6973dd1c217f64b4102f024d588f2fc
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-18 18:40:38 +0100

    Changed over to using tkinter

--- HostFile.py
+++ HostFile.py
@@ -1,8 +1,9 @@
 # Attempting to make a basic chess game whereby the computer randomly selects a next move.
 # This will form the framework for a later project that looks to pit a manual algorithm against a neural network (probably NEAT)
+# The original plan was to use matplotlib but for ease tkinter will be used instead as this makes adding controls very simple
 
 import numpy as np
-import matplotlib.pyplot as plt
+import tkinter as tk
 import pandas as pd
 
 # Some variables to set the chess board apprearance
