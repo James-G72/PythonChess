ab3a8963dc6c20e1542d18acd43dd5798b132503
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-18 16:03:47 +0100

    Added the board as an image

--- HostFile.py
+++ HostFile.py
@@ -7,4 +7,9 @@ import pandas as pd
 
 # Some variables to set the chess board apprearance
 boardSize = 8 # This is a square size (boardSize x boardSize)
-
+boardImage = np.zeros((boardSize,boardSize)) # Store the board in an array
+boardImage[1::2,0::2] = 1 # Every other row has every other line as a 1
+boardImage[0::2,1::2] = 1 # The opposite with an offset
+row_label = col_label = range(boardSize) # creating some labels
+plt.imshow(boardImage,cmap="binary") # Plotting the board with black and white squares
+plt.show()
