deb5cfdc5fc3e05d1792be6d3b1ad8a103d41216
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-18 13:16:46 +0100

    Start before lunch

--- HostFile.py
+++ HostFile.py
@@ -1,7 +1,10 @@
 # Attempting to make a basic chess game whereby the computer randomly selects a next move.
-# This will form the framework for a later project that looks to pit a manual algorythm against a neaural network (probably NEAT)
+# This will form the framework for a later project that looks to pit a manual algorithm against a neural network (probably NEAT)
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Some variables to set the chess board apprearance
+boardSize = 8 # This is a square size (boardSize x boardSize)
 
