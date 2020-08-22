20acf65a1efcef269dde3015e504afc857dd89ae
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-23 00:47:53 +0100

    Added in the move checking for the king piece

--- ChessPieces/king_piece.py
+++ ChessPieces/king_piece.py
@@ -26,7 +26,20 @@ class King():
         self.turns += 1
 
     def updatemoves(self,row,col,boardarray,colourarray):
-        t = 1
+        # The king is possibly the simplest piece to move
+        # Castling hasn't been implemented yet
+        self.moves = pd.DataFrame(np.zeros((8,8)),index=[0,1,2,3,4,5,6,7],columns=[0,1,2,3,4,5,6,7])
+        for row_dif in [-1,0,1]:  # Going up from the row and then down
+            for col_dif in [-1,0,1]:  # Going up from the column and then down
+                comp = row_dif==0 and col_dif==0
+                if 0 <= row+row_dif <= 7 and 0 <= col+col_dif <= 7 and not comp:
+                    if colourarray.loc[row+row_dif,col+col_dif] == 0:  # The square is free
+                        self.moves.loc[row+row_dif,col+col_dif] = 1
+                    elif colourarray.loc[row+row_dif,col+col_dif] == self.colour:
+                        # Do nothing
+                        t = 1
+                    else:
+                        self.moves.loc[row+row_dif,col+col_dif] = 1
 
     def validsquares(self):
         # This method packs the current valid moves into a simple
