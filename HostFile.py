844b1d71a57a9c90025b81302730b034ddb08505
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-23 12:45:48 +0100

    Tidying up the host file with comments and removing unecessary lines

--- HostFile.py
+++ HostFile.py
@@ -3,17 +3,20 @@ import center_tk_window as ctw
 # Importing the custom chess piece classes and the board
 import ChessComponents
 
+# All of the visuals is included in the ChessComponents/board.py file. The class in this file called GameBoard hosts the game.
+
 # Initialising the board
 playWindow = tk.Tk() # Root window is created
-playWindow.title("Chess Game")
-side_size = 200 # This affects the amount of space on the right of the board
+playWindow.title("Chess Game") # Title added
+side_size = 200 # This affects the amount of space on the right of the board (200 is needed)
 board = ChessComponents.GameBoard(playWindow,side_size=side_size) # Initialising the game board within the root window
-board.pack(side="top", fill="both", expand="true", padx=0, pady=0) # Packing and displaying
-playWindow.resizable(width=False, height=False) # This is a very important line that stops the window being edited which makes the layout much easier
-# Setting the geometry to include all of the buttons.
-# I will need to find a better way of doing this
-playWindow.geometry(str(board.size*8+side_size)+"x"+str(board.size*8))
-ctw.center_on_screen(playWindow) # This is a nifty module that centers the window for us
-board.defaults() # Setting the board for the start of a game
+board.pack(side="top", fill="both", expand="true", padx=0, pady=0) # Packing and displaying (in TkInter everything to be displayed in a window needs to be either "packed" or "placed"
+playWindow.resizable(width=False, height=False) # This locks the size of the window so it cant be resized
+playWindow.geometry(str(board.size*8+side_size)+"x"+str(board.size*8)) # This locks the geometry including side_size to encompass the visuals
+ctw.center_on_screen(playWindow) # This is a nifty module that centers the window for us. There might be a better way to do it but it happens once at the start of the game
+board.defaults() # This actually initialises the chess game and adds all of the pieces etc
 
-playWindow.mainloop() # Main loop is set here
+# As with most GUIs the game runs out of the host object which in this case is a GameBoard called board.
+# All the logic required to run a game of chess is included in either board or the chess piece objects it contains.
+# By calling mainloop() on the tkinter window we allow the buttons to run the game with no further code required here,
+playWindow.mainloop()
