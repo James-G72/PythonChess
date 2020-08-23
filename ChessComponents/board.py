a5f11aeb6a2702a3351b314a706d7008cec0f934
James Gower <66065325+James-G72@users.noreply.github.com>
2020-08-23 12:37:56 +0100

    Added base64, math and ChessComponents

--- ChessComponents/board.py
+++ ChessComponents/board.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import pandas as pd
 import numpy as np
+import base64
+import ChessComponents
+import math
 
 class GameBoard(tk.Frame):
     def __init__(self, parent, side_size, square_size=80, rows=8, columns=8, color1="#ffffff", color2="#474747"):
